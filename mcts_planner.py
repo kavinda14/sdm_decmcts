f7654479cca6209cb12f271503794e1d84fef282
Joshua R Bhagat Smith <bhagatsj@oregonstate.edu>
2021-03-03 13:46:18 -0800

    Fewer iterations

--- mcts_planner.py
+++ mcts_planner.py
@@ -11,7 +11,7 @@ from mcts import mcts
 def mcts_planner(robot, map):
     # Solve it with MCTS
     budget = 250
-    max_iterations = 10000
+    max_iterations = 1000
     exploration_exploitation_parameter = .2 # =1.0 is recommended. <1.0 more exploitation. >1.0 more exploration.
     [path, list_of_all_nodes, winner] = mcts(budget, max_iterations, exploration_exploitation_parameter, robot, map)
 
