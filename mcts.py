e4cdb17cce5defd41031d4b28850d64bbaa0b4c1
puriiii <49798864+puriiii@users.noreply.github.com>
2021-03-07 11:45:58 -0800

    Merge remote-tracking branch 'origin/master'

--- mcts.py
+++ mcts.py
@@ -89,155 +89,158 @@ def dec_mcts(budget, mcts_max_number_of_samples, computational_budget, explorati
             # Grow Tree for each Robot
             for robot in robots:
                 # Robots determines their top 10 best sets of actions(sequences)
+                # Selection and Expansion
+                # move recursively down the tree from root
+                # then add a new leaf node
+                # print("Selection and Expansion")
+                current = robot.root
+                robot.reset_robot()
 
+                while True:
+                    # Are there any children to be added here?
+                    if current.unpicked_child_actions:  # if not empty
+                        # Pick one of the children that haven't been added | Select a valid direction
+                        num_unpicked_child_actions = len(current.unpicked_child_actions)
+                        child_index = random.randint(0, num_unpicked_child_actions - 1)
+                        child_action = current.unpicked_child_actions[child_index]
 
-                    # Selection and Expansion
-                    # move recursively down the tree from root
-                    # then add a new leaf node
-                    # print("Selection and Expansion")
-                    current = robot.root
-                    robot.reset_robot()
+                        # Move the robot
+                        # Remove the child form the unpicked list
+                        del current.unpicked_child_actions[child_index]
 
-                    while True:
-                        # Are there any children to be added here?
-                        if current.unpicked_child_actions:  # if not empty
-                            # Pick one of the children that haven't been added | Select a valid direction
-                            num_unpicked_child_actions = len(current.unpicked_child_actions)
-                            child_index = random.randint(0, num_unpicked_child_actions - 1)
-                            child_action = current.unpicked_child_actions[child_index]
+                        # Setup the new action sequence
+                        new_sequence = copy.deepcopy(current.sequence)
+                        new_sequence.append(child_action)
+                        new_budget_left = budget - cost(new_sequence)
 
-                            # Move the robot
-                            # Remove the child form the unpicked list
-                            del current.unpicked_child_actions[child_index]
+                        # Setup the new child's unpicked children
+                        # Remove any over budget or invalid children from this set
+                        new_unpicked_child_actions = generate_neighbors(child_action, new_sequence,
+                                                                        world_map.bounds)
 
-                            # Setup the new action sequence
-                            new_sequence = copy.deepcopy(current.sequence)
-                            new_sequence.append(child_action)
-                            new_budget_left = budget - cost(new_sequence)
+                        def node_is_valid(a):
+                            seq_copy = copy.deepcopy(current.sequence)
+                            seq_copy.append(a)
+                            end_loc = seq_copy[-1].location
+                            over_budget = (cost(seq_copy) > budget)
 
-                            # Setup the new child's unpicked children
-                            # Remove any over budget or invalid children from this set
-                            new_unpicked_child_actions = generate_neighbors(child_action, new_sequence,
-                                                                            world_map.bounds)
+                            return not over_budget
 
-                            def node_is_valid(a):
-                                seq_copy = copy.deepcopy(current.sequence)
-                                seq_copy.append(a)
-                                end_loc = seq_copy[-1].location
-                                over_budget = (cost(seq_copy) > budget)
+                        # removes any new children if from the child if they go over budget or are invalid action
+                        new_unpicked_child_actions = [a for a in new_unpicked_child_actions if node_is_valid(a)]
 
-                                return not over_budget
+                        ##EXPANSION
+                        new_child_node = TreeNode(parent=current, sequence=new_sequence,
+                                                        budget=new_budget_left,
+                                                        unpicked_child_actions=new_unpicked_child_actions,
+                                                        node_id = current.node_id + 1)
 
-                            # removes any new children if from the child if they go over budget or are invalid action
-                            new_unpicked_child_actions = [a for a in new_unpicked_child_actions if node_is_valid(a)]
-
-                            ##EXPANSION
-                            new_child_node = TreeNode(parent=current, sequence=new_sequence,
-                                                            budget=new_budget_left,
-                                                            unpicked_child_actions=new_unpicked_child_actions,
-                                                            node_id = current.node_id + 1)
-
-                            current.children.append(new_child_node)
-                            current = new_child_node
-                            # list_of_all_nodes.append(new_child_node)  # for debugging only
-                            break  # don't go deeper in the tree...
+                        current.children.append(new_child_node)
+                        current = new_child_node
+                        # list_of_all_nodes.append(new_child_node)  # for debugging only
+                        break  # don't go deeper in the tree...
+                    else:
+                        # All possible children already exist
+                        # Therefore recurse down the tree
+                        # using the UCT selection policy
+                        if not current.children:
+                            # Reached planning horizon -- just do this again
+                            break
                         else:
-                            # All possible children already exist
-                            # Therefore recurse down the tree
-                            # using the UCT selection policy
+                            # Define the UCB
+                            def ucb(average, n_parent, n_child):
+                                return average + exploration_exploitation_parameter * math.sqrt(
+                                    (2 * math.log(n_parent)) / float(n_child))
+
+                            # Pick the child that maximises the UCB
                             if not current.children:
-                                # Reached planning horizon -- just do this again
                                 break
                             else:
-                                # Define the UCB
-                                def ucb(average, n_parent, n_child):
-                                    return average + exploration_exploitation_parameter * math.sqrt(
-                                        (2 * math.log(n_parent)) / float(n_child))
+                                n_parent = current.num_updates
+                                best_child = -1
+                                best_ucb_score = 0
+                                for child_idx in range(len(current.children)):
+                                    child = current.children[child_idx]
+                                    ucb_score = ucb(child.average_evaluation_score, n_parent, child.num_updates)
+                                    if best_child == -1 or (ucb_score > best_ucb_score):
+                                        best_child = child
+                                        best_ucb_score = ucb_score
 
-                                # Pick the child that maximises the UCB
-                                if not current.children:
-                                    break
-                                else:
-                                    n_parent = current.num_updates
-                                    best_child = -1
-                                    best_ucb_score = 0
-                                    for child_idx in range(len(current.children)):
-                                        child = current.children[child_idx]
-                                        ucb_score = ucb(child.average_evaluation_score, n_parent, child.num_updates)
-                                        if best_child == -1 or (ucb_score > best_ucb_score):
-                                            best_child = child
-                                            best_ucb_score = ucb_score
+                                # Recurse down the tree
+                                current = best_child
 
-                                    # Recurse down the tree
-                                    current = best_child
+                ################################
+                # Rollout
+                # print("Rollout Phase")
+                rollout_sequence = uniform_rollout(path=current.sequence, robot=robot, budget=budget)
+                # rollout_sequence = heuristic_rollout(path=current.sequence, robot=robot, budget=budget, map=world_map)
+                rollout_reward = reward(action_sequence=rollout_sequence, robot=robot, map=world_map)
 
-                    #Communications
-                    # Sample Action Sequences of other Robots
-                    # Pick one of the 10 sequences
-                    other_robots_paths = []
-                    for bot in robots:
-                        randSequence = randint(0, 9)
-                        if bot != robot and computational_budget == 0 :
-                            sampled_action_sequence = bot.top_10_sequences[randSequence]
-                            other_robots_paths.append(sampled_action_sequence)
+                ################################
+                # Back-propagation
+                # update stats of all nodes from current back to root node
+                # print("Back-Propagation Phase")
+                parent = current
+                while parent:  # is not None
+                    # Update the average
+                    parent.updateAverage(rollout_reward)
+                    # Recurse up the tree
+                    parent = parent.parent
 
-                    ################################
-                    # Rollout
-                    # print("Rollout Phase")
-                    rollout_sequence = uniform_rollout(path=current.sequence, robot=robot, budget=budget)
-                    # rollout_sequence = heuristic_rollout(path=current.sequence, robot=robot, budget=budget, map=world_map)
-                    rollout_reward = reward(action_sequence=rollout_sequence, robot=robot, map=world_map)
 
-                    ################################
-                    # Back-propagation
-                    # update stats of all nodes from current back to root node
-                    # print("Back-Propagation Phase")
-                    parent = current
-                    while parent:  # is not None
-                        # Update the average
-                        parent.updateAverage(rollout_reward)
-                        # Recurse up the tree
-                        parent = parent.parent
+                #################################
+                # Extract 10 Best Sequences from Current Robot
+                print('Extracting Top 10 Solutions')
+                # list_of_top_10_nodes_sequences = []
+                list_of_top_10_nodes_ids = []
+                list_of_top_10_nodes = []
+                i = 0
+                while i != 10:
+                    current = robot.root
+                    while current.children and all_children_nodes_are_not_in_the_list(current,
+                                                                                      list_of_top_10_nodes_ids):  # is not empty
+                        # Find the child with best score
+                        best_score = 0
+                        best_child = -1
+                        for child_idx in range(len(current.children)):
+                            child = current.children[child_idx]
+                            # Only consider child nodes who have not been placed in the list - pat
+                            if not child.node_id in list_of_top_10_nodes_ids:
+                                score = child.average_evaluation_score
+                                if best_child == -1 or (score > best_score):
+                                    best_child = child
+                                    best_score = score
+                        current = best_child
 
-        # Extract 10 Best Sequences from Current Robot
-        print('Extracting Top 10 Solutions')
-        list_of_top_10_nodes_sequences = []
-        list_of_top_10_nodes_ids = []
-        list_of_top_10_nodes = []
-        i = 0
-        while i != 10:
-            current = robot.root
-            while current.children and all_children_nodes_are_not_in_the_list(current,
-                                                                              list_of_top_10_nodes_ids):  # is not empty
-                # Find the child with best score
-                best_score = 0
-                best_child = -1
-                for child_idx in range(len(current.children)):
-                    child = current.children[child_idx]
-                    # Only consider child nodes who have not been placed in the list - pat
-                    if not child.node_id in list_of_top_10_nodes_ids:
-                        score = child.average_evaluation_score
-                        if best_child == -1 or (score > best_score):
-                            best_child = child
-                            best_score = score
-                current = best_child
+                    # Append each iteration's node with the best average evaluation score
+                    list_of_top_10_nodes_ids.append(current.node_id)
+                    list_of_top_10_nodes.append(current)
+                    # Append the the node's action sequence into a list
+                    # print('Node: ', current.node_id)
+                    solution = current.sequence
+                    solution = listActionSequence(solution)
+                    solution = direction_path_to_state_path_converter(solution, robot.start_loc)
+                    # print('Solution: ', solution)
+                    top_10_sequences.append(solution)
 
-            # Append each iteration's node with the best average evaluation score
-            list_of_top_10_nodes_ids.append(current.node_id)
-            list_of_top_10_nodes.append(current)
-            # Append the the node's action sequence into a list
-            # print('Node: ', current.node_id)
-            solution = current.sequence
-            solution = listActionSequence(solution)
-            solution = direction_path_to_state_path_converter(solution, robot.start_loc)
-            # print('Solution: ', solution)
-            list_of_top_10_nodes_sequences.append(solution)
-            i += 1
-        # print('Current Robot Top 10 Node List: ', list_of_top_10_nodes)
-        print('Current Robot Top 10 Node Id List: ', list_of_top_10_nodes_ids)
-        print('Current Robot Top 10 Sequences List: ', list_of_top_10_nodes_sequences)
+                    i += 1
 
-        k += 1
+                # print('Current Robot Top 10 Node List: ', list_of_top_10_nodes)
+                print('Current Robot Top 10 Node Id List: ', list_of_top_10_nodes_ids)
+                print('Current Robot Top 10 Sequences List: ', list_of_top_10_nodes_sequences)
+
+                k += 1
+
+            #Communications
+            # Sample Action Sequences of other Robots
+            # Pick one of the 10 sequences
+            other_robots_paths = []
+            for bot in robots:
+                randSequence = randint(0, 9)
+                if bot != robot or computational_budget == 0 :
+                    sampled_action_sequence = bot.top_10_sequences[randSequence]
+                    other_robots_paths.append(sampled_action_sequence)
+
 
     ################################
     # Extract best solution from all the ROBOTS
